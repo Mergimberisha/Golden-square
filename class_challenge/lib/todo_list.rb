b4193025d0180c06724568aed5ba3ad5d6f08ac9
Mergim <mergimberisha0@gmail.com>
2023-04-18 15:13:35 +0100

    todo_list completed, all tests passed

--- class_challenge/lib/todo_list.rb
+++ class_challenge/lib/todo_list.rb
@@ -1,20 +1,25 @@
 class TodoList
   def initialize
+    @list = []
   end
 
   def add(todo) # todo is an instance of Todo
     # Returns nothing
+    @list << todo
   end
 
   def incomplete
     # Returns all non-done todos
+    @list.reject { |todo| todo.done? }
   end
 
   def complete
     # Returns all complete todos
+    @list.select { |todo| todo.done? }
   end
 
   def give_up!
     # Marks all todos as complete
+    @list.map { |todo| todo.mark_done! }
   end
 end
--- class_challenge/spec/integration_todoclass_spec.rb
+++ class_challenge/spec/integration_todoclass_spec.rb
@@ -1,5 +1,43 @@
 require "todo"
 require "todo_list"
 
-describe Todo do
+describe TodoList do
+  context "Multiple Todos added" do
+    it "Should" do
+      todo_1 = Todo.new("Walk the ferrit")
+      todo_2 = Todo.new("Walk the dog")
+      todo_3 = Todo.new("Walk the cat")
+      list = TodoList.new
+      list.add(todo_1)
+      list.add(todo_2)
+      list.add(todo_3)
+      expect(list.incomplete).to eq [todo_1, todo_2, todo_3]
+    end
+
+    it "Should" do
+      todo_1 = Todo.new("Walk the ferrit")
+      todo_2 = Todo.new("Walk the dog")
+      todo_3 = Todo.new("Walk the cat")
+      list = TodoList.new
+      todo_2.mark_done!
+      list.add(todo_1)
+      list.add(todo_2)
+      list.add(todo_3)
+      expect(list.complete).to eq [todo_2]
+    end
+
+    it "Should" do
+      todo_1 = Todo.new("Walk the ferrit")
+      todo_2 = Todo.new("Walk the dog")
+      todo_3 = Todo.new("Walk the cat")
+      list = TodoList.new
+      list.add(todo_1)
+      list.add(todo_2)
+      list.add(todo_3)
+      list.give_up!
+      expect(todo_1.done?).to eq true
+      expect(todo_2.done?).to eq true
+      expect(todo_3.done?).to eq true
+    end
+  end
 end
