ab1de50aa6d437ec35a93e7cc87b4f7c5b1be54f
Mergim <mergimberisha0@gmail.com>
2023-04-20 16:13:34 +0100

    Added integration_spec.rb

---
+++ design_a_class_system/spec/integration_spec.rb
@@ -0,0 +1,15 @@
+require "./lib/diary"
+require "./lib/todo"
+
+RSpec.describe do
+  it "adds all the completed todos to the diary entries" do
+    diary = Diary.new
+    todo = Todo.new
+    todo.add("my todo")
+    todo.marked_complete
+    todo.done?
+    diary.add_todo(todo)
+
+    expect(diary.list_entries).to eq [todo]
+  end
+end
